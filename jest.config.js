b3040bbc78cea3a7edd62dde0ee70d1d662cfdfd
yan <h4rd1i@gmail.com>
2025-05-17 23:28:11 -0300

    Remove e2e & UI test infrastructure and related dependencies
    Eliminated end-to-end and UI testing frameworks (Jest, Playwright, and Supertest) along with their configurations, scripts, and dependencies. Updated project files such as `tsconfig`, `package.json`, and `eslint` to reflect these removals. Simplified Docker Compose setup and README instructions accordingly.

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleFileExtensions: ['js','json','ts'],
-  rootDir: '.',
-  testRegex: '.spec.ts$',
-  collectCoverage: true,
-  coverageDirectory: './coverage',
-};
